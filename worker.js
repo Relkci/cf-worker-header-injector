a8c8c070cdba14b57b6a9118ce978e646618605d
W3bCrak3r <55418990+W3bCrak3r@users.noreply.github.com>
2022-01-08 18:09:20 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,3 +1,4 @@
+// Fixing Content-Security-Policies with Cloudflare Workers
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -11,8 +12,8 @@ async function handleRequest(request) {
 
   response.headers.set('X-Frame-Options', 'SAMEORIGIN');
   response.headers.set('Referrer-Policy','same-origin');
-  response.headers.set('Content-Security-Policy', 'default-src \'self\' \'unsafe-inline\'');
-  response.headers.set('Feature-Policy','camera \'none\'; geolocation \'none\'');
+  response.headers.set('Content-Security-Policy', 'script-src \'self\' \'unsafe-inline\' \'unsafe-eval\' https: domain.com *.domain.com *.googlesyndication.com *.doubleclick.net *.google.com tpc.googlesyndication.com googleads.g.doubleclick.net g.doubleclick.net; img-src https: data:; font-src https: data: domain.com fonts.googleapis.com;');
+  //response.headers.set('Feature-Policy','camera \'none\'; geolocation \'none\'');
   response.headers.set('X-Content-Type-Options','nosniff');
   response.headers.set('X-XSS-Protection','1; mode=block');
   response.headers.set('Permissions-Policy', 'camera=(), geolocation=(), microphone=()');
